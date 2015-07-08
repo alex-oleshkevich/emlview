d4e10911186ae05f96eeab2541d0b997d0decca9
alex.oleshkevich <alex.oleshkevich@gmail.com>
2015-07-08 23:22:03 +0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+    sudo bash `readlink -f $0`
+    exit
+fi
+
+BIN=`readlink -f emlview`
+DESKTOP=`readlink -f emlview.desktop`
+
+cp $BIN /usr/bin
+cp $DESKTOP /usr/share/applications
+
+if [[ ! -d /usr/share/emlview ]] ; then
+    mkdir -p /usr/share/emlview
+fi
+
+cp `readlink -f mainwindow.ui` /usr/share/emlview/mainwindow.ui
+
+cp icon.svg /usr/share/icons/hicolor/16x16/apps/emlview.svg
+cp icon.svg /usr/share/icons/hicolor/24x24/apps/emlview.svg
+cp icon.svg /usr/share/icons/hicolor/32x32/apps/emlview.svg
+cp icon.svg /usr/share/icons/hicolor/48x48/apps/emlview.svg
+cp icon.svg /usr/share/icons/hicolor/64x64/apps/emlview.svg
+cp icon.svg /usr/share/icons/hicolor/96x96/apps/emlview.svg
+cp icon.svg /usr/share/icons/hicolor/128x128/apps/emlview.svg
+cp icon.svg /usr/share/icons/hicolor/256x256/apps/emlview.svg
---
+++ readme.md
@@ -0,0 +1,11 @@
+###emlview   
+A tiny utility to view eml files.  
+
+####installation
+1. install python-pyqt5 package
+2. exec install.sh
+
+#####Usage
+```bash
+emlview <filename>
+```
